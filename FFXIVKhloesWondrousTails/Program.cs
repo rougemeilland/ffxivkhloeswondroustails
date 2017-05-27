7fe104ffd28d94d7e310742663d9e840b3bc98f3
Lunor Kisasage <lunor.kisasage@mbr.nifty.com>
2017-05-27 12:58:11 +0900

    新規作成

---
+++ FFXIVKhloesWondrousTails/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFXIVKhloesWondrousTails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var クロ帳に9マス埋める組み合わせ = WondrousTailsPattern.EnumeratePattern(9).ToArray();
+            var クロ帳に9マス埋めた時点で4ライン以上並ぶ組み合わせ = クロ帳に9マス埋める組み合わせ.Where(p => p.GetLineCount() >= 4).ToArray();
+            var クロ帳に9マス埋めた時点で3ライン並ぶ組み合わせ = クロ帳に9マス埋める組み合わせ.Where(p => p.GetLineCount() == 3).ToArray();
+            var クロ帳に9マス埋めた時点で2ライン並ぶ組み合わせ = クロ帳に9マス埋める組み合わせ.Where(p => p.GetLineCount() == 2).ToArray();
+            var クロ帳に9マス埋めた時点で1ライン並ぶ組み合わせ = クロ帳に9マス埋める組み合わせ.Where(p => p.GetLineCount() == 1).ToArray();
+            var クロ帳に9マス埋めた時点で0ライン並ぶ組み合わせ = クロ帳に9マス埋める組み合わせ.Where(p => p.GetLineCount() == 0).ToArray();
+            Console.WriteLine(string.Format("クロ帳に9マス埋める組み合わせの数＝{0}", クロ帳に9マス埋める組み合わせ.Length));
+            Console.WriteLine(string.Format("クロ帳に9マス埋めた時点で4ライン以上並ぶ組み合わせの数＝{0} ({1:P2})", クロ帳に9マス埋めた時点で4ライン以上並ぶ組み合わせ.Length, (double)クロ帳に9マス埋めた時点で4ライン以上並ぶ組み合わせ.Length / クロ帳に9マス埋める組み合わせ.Length));
+            Console.WriteLine(string.Format("クロ帳に9マス埋めた時点で3ライン並ぶ組み合わせの数＝{0} ({1:P2})", クロ帳に9マス埋めた時点で3ライン並ぶ組み合わせ.Length, (double)クロ帳に9マス埋めた時点で3ライン並ぶ組み合わせ.Length / クロ帳に9マス埋める組み合わせ.Length));
+            Console.WriteLine(string.Format("クロ帳に9マス埋めた時点で2ライン並ぶ組み合わせの数＝{0} ({1:P2})", クロ帳に9マス埋めた時点で2ライン並ぶ組み合わせ.Length, (double)クロ帳に9マス埋めた時点で2ライン並ぶ組み合わせ.Length / クロ帳に9マス埋める組み合わせ.Length));
+            Console.WriteLine(string.Format("クロ帳に9マス埋めた時点で1ライン並ぶ組み合わせの数＝{0} ({1:P2})", クロ帳に9マス埋めた時点で1ライン並ぶ組み合わせ.Length, (double)クロ帳に9マス埋めた時点で1ライン並ぶ組み合わせ.Length / クロ帳に9マス埋める組み合わせ.Length));
+            Console.WriteLine(string.Format("クロ帳に9マス埋めた時点で1ラインも並ばない組み合わせの数＝{0} ({1:P2})", クロ帳に9マス埋めた時点で0ライン並ぶ組み合わせ.Length, (double)クロ帳に9マス埋めた時点で0ライン並ぶ組み合わせ.Length / クロ帳に9マス埋める組み合わせ.Length));
+            Console.WriteLine("9マス埋めた時点で3ライン並ぶ配置=>");
+            foreach (var p in クロ帳に9マス埋めた時点で3ライン並ぶ組み合わせ)
+            {
+                Console.WriteLine(p.GetPresentation());
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+            var クロ帳に7マス埋める組み合わせ = WondrousTailsPattern.EnumeratePattern(7).ToArray();
+            var クロ帳に7マス埋め更に1マス埋める組み合わせ = クロ帳に7マス埋める組み合わせ.SelectMany(p => p.Put().Select(p2 => new { original_pattern = p, rate = 1.0 / 9, new_pattern = p2 }));
+            var クロ帳に7マス埋め更に2マス埋める組み合わせ = クロ帳に7マス埋め更に1マス埋める組み合わせ.SelectMany(item => item.new_pattern.Put().Select(p2 => new { item.original_pattern, rate = item.rate * 1.0 / 8, new_pattern = p2 }));
+            var クロ帳に7マス埋め更に2マス埋めた結果3ライン並ぶ組み合わせ = クロ帳に7マス埋め更に2マス埋める組み合わせ.Where(item => item.new_pattern.GetLineCount() == 3).ToArray();
+            var クロ帳に7マス埋め更に2マス埋めた結果3ライン並ぶ組み合わせとその確率 = クロ帳に7マス埋め更に2マス埋めた結果3ライン並ぶ組み合わせ.GroupBy(item => item.original_pattern).Select(g => new { pattern = g.Key, rate = g.Sum(item => item.rate) }).ToArray();
+            Console.WriteLine(string.Format("クロ帳に7マス埋める組み合わせの数＝{0}", クロ帳に7マス埋める組み合わせ.Length));
+            var rate_array = クロ帳に7マス埋め更に2マス埋めた結果3ライン並ぶ組み合わせとその確率.Select(item => item.rate).Distinct().OrderByDescending(item => item).ToArray();
+            System.Diagnostics.Debug.Assert(rate_array.Length == 3);// 以降のコードはrate_arrayのサイズが3であることを仮定する
+            var クロ帳に7マス埋め更に2マス埋めた結果3ライン並ぶ可能性が少しでも存在する組み合わせ = クロ帳に7マス埋め更に2マス埋めた結果3ライン並ぶ組み合わせとその確率.Where(item => item.rate >= rate_array[2]).ToArray();
+            var クロ帳に7マス埋め更に2マス埋めた結果3ライン並ぶ可能性が中程度以上で存在する組み合わせ = クロ帳に7マス埋め更に2マス埋めた結果3ライン並ぶ組み合わせとその確率.Where(item => item.rate >= rate_array[1]).ToArray();
+            var クロ帳に7マス埋め更に2マス埋めた結果3ライン並ぶ可能性が比較的高く存在する組み合わせ = クロ帳に7マス埋め更に2マス埋めた結果3ライン並ぶ組み合わせとその確率.Where(item => item.rate >= rate_array[0]).ToArray();
+            Console.WriteLine(string.Format("クロ帳に7マス埋めたうち、更に2マス埋めた結果3ライン並ぶ可能性が少しでも存在する組み合わせ＝{0} ({1:P2}), その場合の3ライン並ぶ平均確率＝{2:P2}",
+                                            クロ帳に7マス埋め更に2マス埋めた結果3ライン並ぶ可能性が少しでも存在する組み合わせ.Length,
+                                            (double)クロ帳に7マス埋め更に2マス埋めた結果3ライン並ぶ可能性が少しでも存在する組み合わせ.Length / クロ帳に7マス埋める組み合わせ.Length,
+                                            クロ帳に7マス埋め更に2マス埋めた結果3ライン並ぶ可能性が少しでも存在する組み合わせ.Average(item => item.rate)));
+            Console.WriteLine(string.Format("クロ帳に7マス埋めたうち、更に2マス埋めた結果3ライン並ぶ可能性が中程度以上で存在する組み合わせ＝{0} ({1:P2}), その場合の3ライン並ぶ平均確率＝{2:P2}",
+                                            クロ帳に7マス埋め更に2マス埋めた結果3ライン並ぶ可能性が中程度以上で存在する組み合わせ.Length,
+                                            (double)クロ帳に7マス埋め更に2マス埋めた結果3ライン並ぶ可能性が中程度以上で存在する組み合わせ.Length / クロ帳に7マス埋める組み合わせ.Length,
+                                            クロ帳に7マス埋め更に2マス埋めた結果3ライン並ぶ可能性が中程度以上で存在する組み合わせ.Average(item => item.rate)));
+            Console.WriteLine(string.Format("クロ帳に7マス埋めたうち、更に2マス埋めた結果3ライン並ぶ可能性が比較的高確率で存在する組み合わせ＝{0} ({1:P2}), その場合の3ライン並ぶ平均確率＝{2:P2}",
+                                            クロ帳に7マス埋め更に2マス埋めた結果3ライン並ぶ可能性が比較的高く存在する組み合わせ.Length,
+                                            (double)クロ帳に7マス埋め更に2マス埋めた結果3ライン並ぶ可能性が比較的高く存在する組み合わせ.Length / クロ帳に7マス埋める組み合わせ.Length,
+                                            クロ帳に7マス埋め更に2マス埋めた結果3ライン並ぶ可能性が比較的高く存在する組み合わせ.Average(item => item.rate)));
+            /*
+            foreach (var p in クロ帳に7マス埋め更に2マス埋めた結果3ライン並ぶ組み合わせ)
+            {
+                Console.WriteLine(string.Format("{0:P2}", p.rate));
+                Console.WriteLine(p.pattern.GetPresentation());
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+            */
+            Console.WriteLine("Ok.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FFXIVKhloesWondrousTails/WondrousTailsPattern.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFXIVKhloesWondrousTails
+{
+    internal class WondrousTailsPattern
+        : IEquatable<WondrousTailsPattern>
+    {
+        #region プライベートフィールド
+
+        // Ａ〇〇Ｂ
+        // 〇〇〇〇
+        // 〇〇〇〇
+        // Ｃ〇〇Ｄ
+        // 上記のクロ帳で、Aはbit0、Bはbit3、Cはbit12、Dはbit15とする。
+
+        private int _bit_pattern;
+
+        #endregion
+
+        #region コンストラクタ
+
+        public WondrousTailsPattern(int bit_pattern)
+        {
+            _bit_pattern = bit_pattern;
+        }
+
+        #endregion
+
+        #region パブリックメソッド
+
+        public static IEnumerable<WondrousTailsPattern> EnumeratePattern(int bit_count)
+        {
+            return (Enumerable.Range(0, 0x10000).Select(bits => new WondrousTailsPattern(bits)).Where(pattern => pattern.GetTotalBitCount() == bit_count));
+        }
+
+        public int GetTotalBitCount()
+        {
+            return (GetBitCount4((_bit_pattern >> 12) & 0b1111) +
+                    GetBitCount4((_bit_pattern >> 8) & 0b1111) +
+                    GetBitCount4((_bit_pattern >> 4) & 0b1111) +
+                    GetBitCount4((_bit_pattern >> 0) & 0b1111));
+        }
+
+        public int GetLineCount()
+        {
+            return ((IsLine(0b0000000000001111) ? 1 : 0) +
+                    (IsLine(0b0000000011110000) ? 1 : 0) +
+                    (IsLine(0b0000111100000000) ? 1 : 0) +
+                    (IsLine(0b1111000000000000) ? 1 : 0) +
+                    (IsLine(0b0001000100010001) ? 1 : 0) +
+                    (IsLine(0b0010001000100010) ? 1 : 0) +
+                    (IsLine(0b0100010001000100) ? 1 : 0) +
+                    (IsLine(0b1000100010001000) ? 1 : 0) +
+                    (IsLine(0b1000010000100001) ? 1 : 0) +
+                    (IsLine(0b0001001001001000) ? 1 : 0));
+        }
+
+        public bool IsLine(int mask)
+        {
+            return ((_bit_pattern & mask) == mask);
+        }
+
+        public string GetPresentation()
+        {
+            return (GetBitPresentation(1 << 0) + GetBitPresentation(1 << 1) + GetBitPresentation(1 << 2) + GetBitPresentation(1 << 3) + "\n" +
+                    GetBitPresentation(1 << 4) + GetBitPresentation(1 << 5) + GetBitPresentation(1 << 6) + GetBitPresentation(1 << 7) + "\n" +
+                    GetBitPresentation(1 << 8) + GetBitPresentation(1 << 9) + GetBitPresentation(1 << 10) + GetBitPresentation(1 << 11) + "\n" +
+                    GetBitPresentation(1 << 12) + GetBitPresentation(1 << 13) + GetBitPresentation(1 << 14) + GetBitPresentation(1 << 15));
+        }
+
+        public IEnumerable<WondrousTailsPattern> Put()
+        {
+            return (Enumerable.Range(0, 16).Select(bit_index => 1 << bit_index).Where(mask => (_bit_pattern & mask) == 0).Select(mask => new WondrousTailsPattern(_bit_pattern | mask)));
+        }
+
+        #endregion
+
+        #region プライベートメソッド
+
+        private int GetBitCount4(int bit_pattern_4)
+        {
+            switch (bit_pattern_4)
+            {
+                case 0b0000:
+                    return (0);
+                case 0b0001:
+                    return (1);
+                case 0b0010:
+                    return (1);
+                case 0b0011:
+                    return (2);
+                case 0b0100:
+                    return (1);
+                case 0b0101:
+                    return (2);
+                case 0b0110:
+                    return (2);
+                case 0b0111:
+                    return (3);
+                case 0b1000:
+                    return (1);
+                case 0b1001:
+                    return (2);
+                case 0b1010:
+                    return (2);
+                case 0b1011:
+                    return (3);
+                case 0b1100:
+                    return (2);
+                case 0b1101:
+                    return (3);
+                case 0b1110:
+                    return (3);
+                case 0b1111:
+                    return (4);
+                default:
+                    throw new ArgumentException();
+            }
+        }
+
+        private string GetBitPresentation(int mask)
+        {
+            return ((_bit_pattern & mask) != 0 ? "●" : "〇");
+        }
+
+        #endregion
+
+        #region object から継承されたメンバ
+
+        public override bool Equals(object o)
+        {
+            if (o == null || GetType() != o.GetType())
+                return (false);
+            return (Equals((WondrousTailsPattern)o));
+        }
+
+        public override int GetHashCode()
+        {
+            return (_bit_pattern.GetHashCode());
+        }
+
+        #endregion
+
+        #region IEquatable<WondrousTailsPattern> のメンバ
+
+        public bool Equals(WondrousTailsPattern o)
+        {
+            if (!_bit_pattern.Equals(o._bit_pattern))
+                return (false);
+            return (true);
+        }
+
+        #endregion
+
+    }
+}
